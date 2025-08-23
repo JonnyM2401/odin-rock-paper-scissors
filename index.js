984dae09f1792d8c0b857c9c3ffe3de69cd26ba3
JonnyM2401 <jonas-melvin.bach@web.de>
2025-08-23 20:41:19 +0200

    Add blueprint for index.html and index.js.

---
+++ index.js
@@ -0,0 +1,18 @@
+// get coice of computer
+    // getComputerChoice()
+    // use Math.random()
+    // test output
+
+// get human coice
+    // use promt()
+    // make it case-insensitive
+    // test output
+
+// create humanScore & computerScore, initValue = 0;
+
+// play single round
+    // function playRound(humanChoice, computerChoice)
+    // representing the round winner, such as: “You lose! Paper beats Rock”.
+    // update Score
+
+// follow next steps
